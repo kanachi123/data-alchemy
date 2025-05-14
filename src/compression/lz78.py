dd6d847a9121845d2b6ccd990d95add490140591
kanachi123 <shrekkanachyan1234@gmail.com>
2025-05-15 03:06:41 +0400

    lz78 1.1

--- src/compression/lz78.py
+++ src/compression/lz78.py
@@ -9,17 +9,36 @@ def lz78_compress(data):
     buffer = ""
 
     for char in data:
+        
         new_buffer = buffer + char
+        
         if new_buffer in dictionary:
             buffer += new_buffer
+        
         else:
             result.append((dictionary.get(buffer, 0), char))
             dictionary[new_buffer] = index
             index += 1 
             buffer = ""
+
     if buffer:
         result.append((dictionary.get(buffer, 0), ""))
     return result
 
+def lz78_decompress(data):
+    if not data:
+        print("empty")
+        return
+    dictionary = {}
+    result = ""
 
-    
+    for index,char in data:
+        tmp = dictionary.get(index, "")
+        result += tmp + char
+        dictionary[len(dictionary) + 1] = tmp + char
+    return result
+
+text = "BANANANANANANNAANANANANA!!!!!"
+
+print(lz78_compress(text))
+print(lz78_decompress(lz78_compress(text)))
