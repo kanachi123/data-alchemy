046902668f078b3fb5fd4c165901b901e8372719
kanachi123 <shrekkanachyan1234@gmail.com>
2025-05-14 12:01:49 +0400

    bwt 1.1

--- src/compression/bwt.py
+++ src/compression/bwt.py
@@ -3,3 +3,10 @@ def bwt_transform(s):
     rotations = [s[i:] + s[:i] for i in range(len(s))]
     rotations.sort()
     return ''.join(row[-1] for row in rotations)
+
+def bwt_inverse(s):
+    n = len(s)
+    table = [''] * n
+    for _ in range(n):
+        table = sorted(s[i] + table[i] for i in range(n))
+    return table[table.index('$')].replace('$', '')
